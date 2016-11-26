fd9f6b084ae35177e8a8e60c1e0138703eee8dab
sasidhar513 <sasidhar.dulipudi@tcs.com>
2016-11-26 11:24:59 +0530

    Update dp.cpp
    added coin change count function.

--- dp.cpp
+++ dp.cpp
@@ -6,6 +6,21 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+unsigned long long int coinCount(int coins[],int amount,int size)
+{
+	unsigned long long int arr[amount+1][size];
+	for(int i=0;i<amount+1;i++)
+		for(int j=0;j<size;j++)
+			arr[i][j]=1;
+    for(int i=1 ;i<=amount;i++)
+	   for(int j=0;j<size;j++)
+		{
+			unsigned long long int x=((i-coins[j])>=0?arr[i-coins[j]][j]:0);
+			unsigned long long int y=j>=1?arr[i][j-1]:0;
+			arr[i][j]=x+y;
+		}
+	return arr[amount][size-1];
+}
 unsigned long long topDownFib(unsigned long long n,unsigned long long arr[])
 {   if(arr[n]>0)
         return arr[n];
